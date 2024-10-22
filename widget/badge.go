a1f56016799a5bcf5f0bc4fc234fc2161f2f1a70
ErikKalkoken <erik.kalkoken@gmail.com>
2024-10-22 15:21:41 +0200

    Rework badge rendered

--- widget/badge.go
+++ widget/badge.go
@@ -1,11 +1,8 @@
 package widget
 
 import (
-	"fmt"
-
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/canvas"
-	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/theme"
 	"fyne.io/fyne/v2/widget"
 )
@@ -13,27 +10,52 @@ import (
 // Badge is a variant of Fyne label widget that renders as badge.
 // Badges are common UI elements to display counts.
 type Badge struct {
-	widget.BaseWidget
-	label *widget.Label
+	*widget.Label
 }
 
 // NewBadge returns a new instance of a [Badge] widget.
 func NewBadge(text string) *Badge {
-	l := widget.NewLabel(fmt.Sprintf(" %s ", text))
-	l.Alignment = fyne.TextAlignCenter
-	w := &Badge{label: l}
+	w := &Badge{Label: widget.NewLabel(text)}
 	w.ExtendBaseWidget(w)
+	w.SetText(text)
 	return w
 }
 
-// SetText sets the text of a badge.
-func (w *Badge) SetText(text string) {
-	w.label.SetText(fmt.Sprintf(" %s ", text))
+func (w *Badge) CreateRenderer() fyne.WidgetRenderer {
+	r := w.Label.CreateRenderer()
+	b := canvas.NewRectangle(theme.Color(theme.ColorNameInputBackground))
+	b.CornerRadius = 10
+	return &badgeRenderer{WidgetRenderer: r, background: b}
 }
 
-func (w *Badge) CreateRenderer() fyne.WidgetRenderer {
-	r := canvas.NewRectangle(theme.Color(theme.ColorNameInputBackground))
-	r.CornerRadius = 10
-	c := container.NewStack(container.NewPadded(r), w.label)
-	return widget.NewSimpleRenderer(c)
+type badgeRenderer struct {
+	fyne.WidgetRenderer
+
+	background *canvas.Rectangle
+}
+
+func (r *badgeRenderer) Layout(size fyne.Size) {
+	topLeft := fyne.NewPos(0, 0)
+	objs := r.Objects()
+	bg := objs[0]
+	label := objs[1]
+
+	s := label.MinSize()
+	label.Resize(s)
+	label.Move(topLeft)
+	bg.Resize(s)
+	bg.Move(topLeft)
+}
+
+func (r *badgeRenderer) MinSize() fyne.Size {
+	minSize := fyne.NewSize(0, 0)
+	for _, child := range r.Objects() {
+		minSize = minSize.Max(child.MinSize())
+	}
+	return minSize
+}
+
+func (r *badgeRenderer) Objects() []fyne.CanvasObject {
+	objs := []fyne.CanvasObject{r.background, r.WidgetRenderer.Objects()[0]}
+	return objs
 }
